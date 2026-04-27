e92a139a3b43c9715b4399588b088d221f948921
Marco Optimus <paul.ramz247@gmail.com>
2026-04-27 14:08:25 -0600

    Projects section DONE!

--- frontend/src/assets/data/projects.js
+++ frontend/src/assets/data/projects.js
@@ -1,4 +1,5 @@
-import fandomIcon from '../icons/fandom-icon.svg';
+import { GiHeartburn } from 'react-icons/gi';
+import { SiCrunchyroll } from 'react-icons/si';
 
 export const projects = [
   {
@@ -8,13 +9,13 @@ export const projects = [
     title: 'Chainsaw Man',
     image:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9EQaXgUxKHvXIBipSMa73NBwlhdGom5MV2g&s',
-    releaseYear: 2022 + ' - ' + 2026,
+    releaseYear: '2022 - 2026',
     streamingName: 'Crunchy Roll',
     streamingUrl:
       'https://www.crunchyroll.com/es/series/GVDHX8QNW/chainsaw-man?srsltid=AfmBOoqLJhwJ5n09fUJw6SxQCVj6lpQaC9ck7OMHN-LlkYtY_6bcVpWe',
-    streamingIcon: null,
+    streamingIcon: SiCrunchyroll,
     wikiUrl: 'https://doblaje.fandom.com/es/wiki/Kobeni_Higashiyama',
-    wikiIcon: fandomIcon,
+    wikiIcon: GiHeartburn,
     studioName: 'Audiomaster Candiani',
     studioUrl: 'https://www.audiomaster.com.mx/',
   },
@@ -25,13 +26,13 @@ export const projects = [
     title: 'Do It Yourself!!',
     image:
       'https://shikimori.io/uploads/poster/characters/194643/main-19d6fbd066f84d8b13b0c8cca398c7c4.webp',
-    releaseYear: 2022 + ' - ' + 2023,
+    releaseYear: '2022 - 2023',
     streamingName: 'Crunchy Roll',
     streamingUrl:
       'https://www.crunchyroll.com/es/series/GEXH3W4JP/do-it-yourself?srsltid=AfmBOoob5Ro0KNaXfzawhHsQ6qrQPZVk6Kf9Uaa9N_XTnIDl-mS41U-M',
-    streamingIcon: null,
+    streamingIcon: SiCrunchyroll,
     wikiUrl: 'https://doblaje.fandom.com/es/wiki/Do_It_Yourself!#Reparto',
-    wikiIcon: fandomIcon,
+    wikiIcon: GiHeartburn,
     studioName: 'Audiomaster Candiani',
     studioUrl: 'https://www.audiomaster.com.mx/',
   },
@@ -41,14 +42,14 @@ export const projects = [
     character: 'Izumo Kamiki',
     title: 'Blue Exorcist',
     image:
-      'https://static.wikia.nocookie.net/doblaje/images/a/a0/Izumo_Kamiki_pose%C3%ADda_-_BE.png/revision/latest/scale-to-width-down/1000?cb=20260313135914&path-prefix=es',
-    releaseYear: 2025 + ' - ' + 2026,
+      'https://i.pinimg.com/474x/8c/bd/3b/8cbd3b170fbdb175f418e937ae18dba5.jpg',
+    releaseYear: '2025 - 2026',
     streamingName: 'Crunchy Roll',
     streamingUrl:
       'https://www.crunchyroll.com/es/series/G649PJ0JY/blue-exorcist?srsltid=AfmBOorfzqmpA-F2W5ef79xyDz1gbokjtYFs9P1oMi7qdr-I1qMjlBDE',
-    streamingIcon: null,
+    streamingIcon: SiCrunchyroll,
     wikiUrl: 'https://doblaje.fandom.com/es/wiki/Blue_Exorcist#Reparto',
-    wikiIcon: fandomIcon,
+    wikiIcon: GiHeartburn,
     studioName: 'Audiomaster Candiani',
     studioUrl: 'https://www.audiomaster.com.mx/',
   },
@@ -58,14 +59,14 @@ export const projects = [
     character: 'Machina Soleige',
     title: 'Demon Lord 2099',
     image:
-      'https://static.wikia.nocookie.net/doblaje/images/6/64/Machina_Soleige_%28Pilar%29_%28DL2099%29.jpg/revision/latest?cb=20241130233620&path-prefix=es',
-    releaseYear: 2024 + ' - ' + 2025,
+      'https://i.pinimg.com/736x/70/0e/78/700e78d77eb4422be1a5d9176770a9bb.jpg',
+    releaseYear: '2024 - 2025',
     streamingName: 'Crunchy Roll',
     streamingUrl:
       'https://www.crunchyroll.com/es/series/G649PJ0JY/blue-exorcist?srsltid=AfmBOorfzqmpA-F2W5ef79xyDz1gbokjtYFs9P1oMi7qdr-I1qMjlBDE',
-    streamingIcon: null,
+    streamingIcon: SiCrunchyroll,
     wikiUrl: 'https://doblaje.fandom.com/es/wiki/Demon_Lord_2099#Reparto',
-    wikiIcon: fandomIcon,
+    wikiIcon: GiHeartburn,
     studioName: 'Audiomaster Candiani',
     studioUrl: 'https://www.audiomaster.com.mx/',
   },
@@ -75,17 +76,17 @@ export const projects = [
     character: 'Lihanna Owenzaus',
     title: 'Wistoria: Wand and Sword',
     image:
-      'https://static.wikia.nocookie.net/doblaje/images/f/fd/Lihanna_Owenzaus_%28W-W%26S%29.jpg/revision/latest?cb=20240929233532&path-prefix=es',
-    releaseYear: 2024 + ' - ' + 2026,
+      'https://i.pinimg.com/originals/12/28/57/12285746baca3e55e9931fa8ef637220.jpg',
+    releaseYear: '2024 - 2026',
     streamingName: 'Crunchy Roll',
     streamingUrl:
       'https://www.crunchyroll.com/es/series/GW4HM7WK9/wistoria-wand-and-sword?srsltid=AfmBOop_h-QfmK9xkR6eHFrdt7nR6m7qTAcg97a4Cq6sK34Z2mmqs6rD',
-    streamingIcon: null,
+    streamingIcon: SiCrunchyroll,
     wikiUrl:
       'https://doblaje.fandom.com/es/wiki/Wistoria:_Wand_and_Sword#Reparto',
-    wikiIcon: fandomIcon,
-    studioName: '',
-    studioUrl: '',
+    wikiIcon: GiHeartburn,
+    studioName: 'Audiomaster Candiani',
+    studioUrl: 'https://www.audiomaster.com.mx/',
   },
   {
     id: 6,
@@ -93,31 +94,31 @@ export const projects = [
     character: 'Hellko',
     title: 'Love After World Domination',
     image:
-      'https://static.wikia.nocookie.net/doblaje/images/a/a2/Hellko_%28LaWD%29.png/revision/latest?cb=20220709033331&path-prefix=es',
-    releaseYear: 2022,
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT2-qEsedSnkiAzEU5CmDNJ9QY4UKTw7h9kg&s',
+    releaseYear: '2022',
     streamingName: 'Crunchy Roll',
     streamingUrl:
       'https://www.crunchyroll.com/es/series/GEXH3WG78/love-after-world-domination?srsltid=AfmBOorfpPMy4nEufn6peAllvgjb_KSwLOEBOHryu4j5voM30z2dPPG_',
-    streamingIcon: null,
+    streamingIcon: SiCrunchyroll,
     wikiUrl:
       'https://doblaje.fandom.com/es/wiki/El_amor_tras_la_dominación_mundial#Reparto',
-    wikiIcon: fandomIcon,
+    wikiIcon: GiHeartburn,
     studioName: 'Audiomaster Candiani',
     studioUrl: 'https://www.audiomaster.com.mx/',
   },
-  {
-    id: 0,
-    type: 'direcciones',
-    character: '',
-    title: '',
-    image: '',
-    releaseYear: 0,
-    streamingName: '',
-    streamingUrl: '',
-    streamingIcon: null,
-    wikiUrl: '',
-    wikiIcon: fandomIcon,
-    studioName: '',
-    studioUrl: '',
-  },
+  // {
+  //   id: 0,
+  //   type: 'direcciones',
+  //   character: '',
+  //   title: '',
+  //   image: '',
+  //   releaseYear: 0,
+  //   streamingName: '',
+  //   streamingUrl: '',
+  //   streamingIcon: null,
+  //   wikiUrl: '',
+  //   wikiIcon: GiHeartburn,
+  //   studioName: '',
+  //   studioUrl: '',
+  // },
 ];
--- frontend/src/assets/sections/Projects.jsx
+++ frontend/src/assets/sections/Projects.jsx
@@ -1,7 +1,137 @@
+import { useState } from 'react';
 import { projects } from '../data/projects';
+import { BiMovie } from 'react-icons/bi';
+import { GiCharacter } from 'react-icons/gi';
+import { FaRegCalendarAlt } from 'react-icons/fa';
+import './Projects.css';
 
 const Projects = () => {
-  return <section id="proyectos" className="projects"></section>;
+  const [activeTab, setActiveTab] = useState('doblaje');
+  const filteredProjects = projects.filter(
+    (project) => project.type === activeTab,
+  );
+
+  return (
+    <section id="proyectos" className="projects">
+      <div className="projects-header">
+        <h2 className="projects-header__title">Proyectos</h2>
+        <p className="projects-header__description">
+          Aquí tienes algunos de mis proyectos más recientes.
+        </p>
+      </div>
+
+      <div className="projects-selector">
+        <button
+          className={`projects-tab ${activeTab === 'doblaje' ? 'active' : ''}`}
+          onClick={() => setActiveTab('doblaje')}
+        >
+          Doblaje
+        </button>
+        <button
+          className={`projects-tab ${activeTab === 'direcciones' ? 'active' : ''}`}
+          onClick={() => setActiveTab('direcciones')}
+        >
+          Direcciones
+        </button>
+      </div>
+
+      <div className="projects-content">
+        {filteredProjects.length === 0 ? (
+          <p className="projects-no-results">
+            No hay proyectos para esta categoría.
+          </p>
+        ) : (
+          filteredProjects.map((project) => {
+            const StreamingIcon = project.streamingIcon;
+            const WikiIcon = project.wikiIcon;
+
+            return (
+              <div key={project.id} className="project-card">
+                <div className="project-card__image-container">
+                  <img
+                    src={project.image}
+                    alt={project.title}
+                    className="project-card__image"
+                  />
+                  <div className="project-card__info">
+                    <div className="project-card__info-container">
+                      <div className="project-card__title-container">
+                        <BiMovie
+                          size={20}
+                          className="project-card__title-icon"
+                        />
+                        <h3 className="project-card__title">{project.title}</h3>
+                      </div>
+
+                      <div className="project-card__character-container">
+                        <GiCharacter
+                          size={20}
+                          className="project-card__character-icon"
+                        />
+                        <p className="project-card__character">
+                          {project.character}
+                        </p>
+                      </div>
+
+                      <div className="project-card__release-year-container">
+                        <FaRegCalendarAlt
+                          size={20}
+                          className="project-card__release-year-icon"
+                        />
+                        <p className="project-card__release-year">
+                          {project.releaseYear}
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="project-card__links">
+                      <div className="project-card__streaming-container">
+                        <StreamingIcon size={20} />
+                        <a
+                          href={project.streamingUrl}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="project-card__streaming-link"
+                        >
+                          Ver en streaming
+                        </a>
+                      </div>
+
+                      <div className="project-card__wiki-container">
+                        <WikiIcon size={20} />
+                        <a
+                          href={project.wikiUrl}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="project-card__wiki-link"
+                        >
+                          Ver en Wiki
+                        </a>
+                      </div>
+
+                      <div className="project-card__studio-container">
+                        <p className="project-card__studio-description">
+                          Estudio de doblaje:
+                        </p>
+                        <a
+                          href={project.studioUrl}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="project-card__studio-link"
+                        >
+                          {project.studioName}
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
